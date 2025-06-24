d43742c6fa3702bf92abc6ca1a53c7a6d5d6115f
Imoleayo Olunde <154096052+Emsy-T@users.noreply.github.com>
2025-06-24 11:45:10 +0100

    Increase top padding for Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -232,7 +232,7 @@ export default function Portfolio() {
       </nav>
 
       {/* Hero Section */}
-      <section className='pt-24 pb-2 px-4 sm:px-6 lg:px-8'>
+      <section className='pt-30 pb-2 px-4 sm:px-6 lg:px-8'>
         <div className='max-w-6xl mx-auto text-center'>
           <motion.div
             style={{ y: heroY }}
